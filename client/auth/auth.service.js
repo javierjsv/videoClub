76c16e7f8c5d557fb44e01bffd0f01924d6d1700
bratc <bramasterweb@gmail.com>
2017-06-12 13:28:51 -0500

    Proyecto video club

---
+++ client/auth/auth.service.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module("videoClubApp")
+.factory('AuthService',AuthService);
+
+AuthService.$inject  = ['$auth','$state'];
+function AuthService($auth,$state){
+	var Auth = {
+		login:login,
+		logout:logout,
+		isAdmin:isAdmin,
+		isAuthenticated:isAuthenticated
+	}
+
+	function login(user,collback){
+		$auth.login(user)
+		.then(response => {
+			console.log("Login ok",response);
+			console.log(Auth.isAdmin());
+			$state.go('main');
+		})
+		.catch(err =>{
+			console.log("Error de login");
+			$state.go('login');
+		})
+	}
+
+	function logout(){
+		if($auth.isAuthenticated()){
+			$auth.logout()
+			.then(respose=>{
+				$state.go('main');
+			})
+		}
+
+	}
+	function isAdmin(){
+		if(Auth.isAuthenticated()){
+				console.log("ROLES",$auth.getPayload().roles);
+				if($auth.getPayload().roles.indexOf("ADMIN") !== -1){
+					return true;
+				}else{
+					return false;
+				}
+		}else{
+			return false;
+		}
+
+	}
+	function isAuthenticated(){
+		if($auth.isAuthenticated()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	return Auth;
+
+}
